4b92e9a941441487498110aa0e7bb63a682db0d3
David <David@192.168.1.105>
2017-08-25 19:21:28 +0200

    First steps of images commands

--- app.js
+++ app.js
@@ -1,57 +1,43 @@
-
-/*
-  A ping pong bot, whenever you send "ping", it replies "pong".
-*/
-
 // Import the discord.js module
 const Discord = require('discord.js');
-
 var schedule = require('node-schedule');
+
+const CHAN_ID_DKC_GENERAL = "349976478538268674";
+const CHAN_ID_QGS_FLAAMCHAN = "330420560972742656";
 
 // Create an instance of a Discord client
 const client = new Discord.Client();
 
-// The token of your bot - https://discordapp.com/developers/applications/me
-
-// The ready event is vital, it means that your bot will only start reacting to information
-// from Discord _after_ ready is emitted
 client.on('ready', () => {
+    client.channels.get(CHAN_ID_DKC_GENERAL).send("Flaambot ready \:heart_eyes_cat: !");
 
-var rule = new schedule.RecurrenceRule();
+    var rule = new schedule.RecurrenceRule();
     rule.minute = 0;
     rule.hour = 6;
-    //rule.second =30;
+    // rule.second =30;
 
     var j = schedule.scheduleJob(rule, function() {
         client.channels.get("349976478538268674").send("Testing");
-        /*client.channels.get("330420560972742656").send({
-                "embed": {
-                        title: 'Photo de Flaam du jour <3',
-                        image: {
-                                "url" : "http://172.104.154.243/pics/today.jpg"
-                        }
-                }
-        });
-        console.log('Pics sent to channel !');*/
+        /*
+		 * client.channels.get("330420560972742656").send({ "embed": { title:
+		 * 'Photo de Flaam du jour <3', image: { "url" :
+		 * "http://172.104.154.243/pics/today.jpg" } } }); console.log('Pics
+		 * sent to channel !');
+		 */
     });
-    client.channels.get("349976478538268674").send("Flaambot ready \:heart_eyes_cat: !");
 });
 
 // Create an event listener for messages
 client.on('message', message => {
-  // If the message is "ping"
-  if (message.content === 'ping') {
-    // Send "pong" to the same channel
-    // message.channel.send('pong');
-  }
 
-
-  // If the message is "ping"
-  if (message.content === 'malo' && message.channel.id == "330420560972742656") {
-    // Send "pong" to the same channel
-    message.channel.send('c\'est le plus beau :)');
-  }
-
+	  if (message.content === '!availableImages') {
+	    // Send "pong" to the same channel
+	    message.channel.send('Here is a list of available images :');
+	  }
+	  if (message.content === '!usedImages') {
+		    // Send "pong" to the same channel
+		    message.channel.send('Here is a list of used images :');
+	  }
 });
 
 // Log our bot in
