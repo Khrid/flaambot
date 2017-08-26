7575e199dc5c0c0be144d31d703746f3b5228535
David <David@192.168.1.105>
2017-08-26 20:42:27 +0200

    Typo again

--- tools.js
+++ tools.js
@@ -25,7 +25,7 @@ function scanDir(dir) {
 function sendToLogChannel(message) {
 	module.exports.client.channels.get(module.exports.CHAN_ID_DKC_FLAAMLOGS).send(message);
 }
-function sendToLogChannel(title, file) {
+function sendImageToLogChannel(title, file) {
 	module.exports.client.channels.get(module.exports.CHAN_ID_DKC_FLAAMLOGS).send(title, {
         files: [
         	file
